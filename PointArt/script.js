19277888248a0a08ff11cdc4f76a1ede7a3ae8a2
Felizardo77 <felizardopaulo40@gmail.com>
2024-07-12 23:06:16 +0200

    update

--- PointArt/script.js
+++ PointArt/script.js
@@ -1,21 +1,3 @@
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("slide");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 5000); // Change image every 5 seconds
-}
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
 
 document.getElementById('menu-toggle').addEventListener('click', function () {
     const navPopup = document.getElementById('nav-popup');
